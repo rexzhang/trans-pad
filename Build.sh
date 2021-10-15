f77e4ef2e3849188e0e1727ab929c28a9e311669
Rex Zhang <rex.zhang@gmail.com>
2021-10-15 16:57:27 +0800

    hdiutil does not support internet-enable. Note it was removed in macOS 10.15.

--- Build.sh
+++ Build.sh
@@ -8,6 +8,5 @@ python setup.py py2app
 python build.py popclip
 
 rm *.dmg
-create-dmg \
-  --no-internet-enable \
-  "TransPad.dmg" "dist"
+# create-dmg --no-internet-enable "TransPad.dmg" "dist"
+create-dmg "TransPad.dmg" "dist"
